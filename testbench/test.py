79412b8b8699b751aafe03a6c65a4a3ff3c66131
owen_geng1 <owengeng@hotmail.com>
2025-01-19 21:04:36 +0000

    first commit

--- testbench/test.py
+++ testbench/test.py
@@ -1,13 +1,13 @@
 import numpy as np
 from vect_classes import World, Vector
+import random
+import cmath
+import time
+import matplotlib.pyplot as plt
+import json
 from math_operations import *
 
-p1 = np.array([1, 3])
-p2 = np.array([2, 5])
-p3 = np.array([5, 9])
+random.seed(43)
 
-l1 = np.array([1, 2, 5])
-l2 = np.array([3, 5, 9])
-
-print(solve_quadratic(p1, p2, p3))
-print(solve_quadratic_2(l1, l2))
+world = World(100)
+line1 = world.generate(1)
