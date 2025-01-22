835eec8d3011301f4b7dd953203938c58077c57f
owen_geng1 <owengeng@hotmail.com>
2025-01-22 12:45:05 +0000

    22_01_2025

--- testbench/angle_test.py
+++ testbench/angle_test.py
@@ -9,7 +9,36 @@ from math_operations import *
 
 random.seed(43)
 
-world = World(3)
-line1 = world.generate(1)
+world = World(10000)
 
-world.generate_angle(line1, 2.5)
+repeat_count = 10
+
+results1 = []
+results2 = []
+angle_arr = []
+
+for angle in range(100, 785, 1):
+    angle_fix = angle/1000
+    total1 = 0
+    total2 = 0
+    for repeats in range(0, repeat_count):
+        line1 = world.generate(1)
+        line2 = world.generate_angle(line1, angle_fix)
+
+        start = time.time()
+        world.orthogonal_descent(line1, line2)
+        end = time.time()
+        total1 = total1 + (end-start)/repeat_count
+
+        start = time.time()
+        world.quadratic_solve(line1, line2)
+        end = time.time()
+        total2 = total2 + (end-start)/repeat_count
+
+    results1.append(total1)
+    results2.append(total2)
+    angle_arr.append(angle_fix)
+
+plt.plot(angle_arr, results1)
+plt.plot(angle_arr, results2)
+plt.show()
